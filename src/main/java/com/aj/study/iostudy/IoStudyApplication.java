0e543ee936d51acbf6381ccdd0216e098cf5085e
zaj <724755391@qq,com>
2020-09-25 14:18:37 +0800

    JUC学习

---
+++ src/main/java/com/aj/study/iostudy/IoStudyApplication.java
@@ -0,0 +1,13 @@
+package com.aj.study.iostudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IoStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aj/study/iostudy/IoStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aj.study.iostudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
